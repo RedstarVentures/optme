d03c5d5c488636a5a0b790afd0855e8ce6de2426
joytafty <joytafty@gmail.com>
2013-05-04 14:11:01 -0400

    test basic views

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -7,7 +7,7 @@ from djangoapp.views import hello, current_datetime, hours_ahead
 urlpatterns = patterns('',
 	url(r'^hello/$', hello),
 	url(r'^time/$', current_datetime),
-	url(r'^time/plus/(\d{1,2}/)$', hours_ahead),
+	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     # Examples:
     # url(r'^$', 'djangoapp.views.home', name='home'),
     # url(r'^djangoapp/', include('djangoapp.foo.urls')),
