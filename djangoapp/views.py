1fa845dadab10b123272249bb655f19fe5be0462
joytafty <joytafty@gmail.com>
2013-05-04 20:50:41 -0400

    respond to request by render shortcuts

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,19 +1,21 @@
-from django.template.loader import get_template
-from django.template import Template, Context
-from django.http import HttpResponse
+# from django.template.loader import get_template
+# from django.template import Template, Context
+# from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 def hello(request):
-	t = get_template('hello.html')
-	html = t.render(Context())	
-	return HttpResponse(html)
+	return render(request, 'hello.html')
+	# t = get_template('hello.html')
+	# html = t.render(Context())	
+	# return HttpResponse(html)
 
 def current_datetime(request):
 	tnow = datetime.datetime.now()
-	t = get_template('current_datetime.html')
-	#t = Template("<html><body> Current time is %s. </body></html>")
-	html = t.render(Context({'current_date': tnow}))
-	return HttpResponse(html)
+	return render(request, 'current_datetime.html', {'current_date': tnow})
+	# t = get_template('current_datetime.html')
+	# html = t.render(Context({'current_date': tnow}))
+	# return HttpResponse(html)
 
 def hours_ahead(request, offset):
 	try:
@@ -21,5 +23,6 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = "<html><body>In %s hour(s), it will be %s. </body></html>" % (offset,dt)
-	return HttpResponse(html)
+	return render(request, 'current_datetime.html', {'current_date': dt})
+	# html = "<html><body>In %s hour(s), it will be %s. </body></html>" % (offset,dt)
+	# return HttpResponse(html)
