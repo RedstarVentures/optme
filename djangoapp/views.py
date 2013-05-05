9f8b6e129f27c46e38304502a2ccd1a8f0c098d0
joytafty <joytafty@gmail.com>
2013-05-04 20:31:54 -0400

    add templates and style (twitter bootstrap)

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,12 +1,18 @@
+from django.template.loader import get_template
+from django.template import Template, Context
 from django.http import HttpResponse
 import datetime
 
 def hello(request):
-    return HttpResponse("Hello visitor")
+	t = get_template('hello.html')
+	html = t.render(Context())	
+	return HttpResponse(html)
 
 def current_datetime(request):
-	now = datetime.datetime.now()
-	html = "<html><body> Current time is %s. </body></html>" % now
+	tnow = datetime.datetime.now()
+	t = get_template('current_datetime.html')
+	#t = Template("<html><body> Current time is %s. </body></html>")
+	html = t.render(Context({'current_date': tnow}))
 	return HttpResponse(html)
 
 def hours_ahead(request, offset):
