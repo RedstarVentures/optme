28c0f9b179c0578054334375f97c425f1b2014c6
joytafty <joytafty@gmail.com>
2013-05-07 23:06:11 -0400

    add app

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -112,13 +112,14 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # #'django.contrib.sites',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
     # 'django.contrib.messages',
     # 'django.contrib.staticfiles',
-    'books',
+    # 'books',
+    'polls',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
